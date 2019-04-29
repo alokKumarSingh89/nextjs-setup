6874acb299ba4ee258a7d4f2acb7b88b4f8e6b14
alokKumarSingh89 <itsaloksingh2011@gmail.com>
2019-04-29 10:28:32 +0530

    learn

---
+++ pages/about.js
@@ -0,0 +1,4 @@
+import withLayout from "../component/Layout";
+
+const About=() => <p>This is the about page</p>
+export default withLayout(About);
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,31 @@
-import Header from "../components/header";
-import withMui from "../shared/MUI/withMUI";
-const Index = ({ title = "Hello nextjs" }) => (
-  <div>
-    <Header />
-    <h2>{title}</h2>
-  </div>
-);
+import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
+import withLayout, {Layout} from "../component/Layout";
 
-export default withMui(Index);
+const Index=props => (
+	<Layout>
+		<h1>Batman tv show</h1>
+		<ul>
+			 {
+				props.show.map(show => (
+					<li key={show.id}>
+						<Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+							<a>{show.name}</a>
+						</Link>
+					</li>
+				))
+			}
+		</ul>
+	</Layout>
+)
+
+Index.getInitialProps=async () =>{
+	const res=await fetch('https://api.tvmaze.com/search/shows?q=batman');
+	const data= await res.json();
+	console.log(`Show data count:${data.length}`);
+	return {
+		show: data.map(entry => entry.show)
+	}
+}
+
+export default Index//withLayout(Index);
